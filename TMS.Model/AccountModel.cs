f6b0824d49ad646397dced450aadcfc1a70a571f
Bryan Schoot <bdjschoot@gmail.com>
2018-04-05 19:46:01 +0200

    TMS: Refactor
    Refactor name of the TMS to TMS.Mvc adding TMS.Console for test purpose.

---
+++ TMS.Model/AccountModel.cs
@@ -0,0 +1,27 @@
+﻿namespace TMS.Model
+{
+    public class AccountModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public AddressModel Address { get; set; }
+        public AccountModel(int id, string email, string password, string firstName, string lastName, int age, AddressModel address)
+        {
+            this.Id = id;
+            this.Email = email;
+            this.Password = password;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Address = address;
+        }
+        public override string ToString()
+        {
+            return $"{Id}\n{Email} : {Password}\n{FirstName} : {LastName} : {Age}\n{Address}";
+        }
+    }
+}
---
+++ TMS.Model/AddressModel.cs
@@ -0,0 +1,25 @@
+﻿namespace TMS.Model
+{
+    public class AddressModel
+    {
+        public int Id { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string CityName { get; set; }
+        public string CountryName { get; set; }
+        public AddressModel(int id, string streetName, string streetNumber, string zipCode, string cityName, string counrtyName)
+        {
+            this.Id = id;
+            this.StreetName = streetName;
+            this.StreetNumber = streetNumber;
+            this.ZipCode = zipCode;
+            this.CityName = cityName;
+            this.CountryName = counrtyName;
+        }
+        public override string ToString()
+        {
+            return $"{Id}\n{StreetName} : {StreetNumber}\n{CityName} : {ZipCode}\n{CountryName}";
+        }
+    }
+}
